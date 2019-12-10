83a1bbb9562beaf5baddf9a809a1de69632402c5
joao front <joao.saousa@tangotech.com.br>
2019-12-10 10:08:00 -0300

    inicio departaments

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -3,10 +3,10 @@ import './Buttom.css'
 
 
 export default props=>{
-let classes = 'button'
-classes  += props.operation ? 'operation' : ''
-classes  += props.double ? 'double' :''
-classes  += props.triple ? 'triple' :''
+let classes = 'buttom'
+classes  += props.operation ? 'operation ' : ''
+classes  += props.double ? '--double ' :''
+classes  += props.triple ? '--triple ' :''
 
 return (
     <button
